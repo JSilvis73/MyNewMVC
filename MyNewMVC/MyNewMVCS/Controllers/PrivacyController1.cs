01f7825e3384a5b85a7fb361e73282908de6c5ad
JSilvis <JSilvis030592@yahoo.com>
2023-02-27 14:43:47 -0500

    This is a setup from scratch of another project in my project.

---
+++ MyNewMVC/MyNewMVCS/Controllers/PrivacyController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyNewMVCS.Controllers
+{
+    public class PrivacyController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
