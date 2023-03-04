d0409e92911ed2970a148da437ca209bf11a94d6
JSilvis <JSilvis030592@yahoo.com>
2023-03-04 13:01:14 -0500

    Added new Subcontroller and Subview

---
+++ MyNewMVC/MyNewMVCS/Controllers/SubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyNewMVCS.Controllers
+{
+    public class SubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
